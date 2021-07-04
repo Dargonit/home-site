a359516ff2183dec54f226348a34c3a58541f149
Dargonit <igrokyse@gmail.com>
2021-07-04 11:48:28 -0700

    moved navbar to component added icon

--- src/App.js
+++ src/App.js
@@ -1,29 +1,10 @@
-import {Route, Switch,Link} from "react-router-dom";
-import './App.css';
-import Home from "./Home";
-import About from "./About";
+import Navbar from "./components/Navbar"
 
 function App() {
   return (
     <div>
       <header >
-        <nav>
-        <ul className="menu">
-          <li className="logo">icon</li>
-          <li className="item"> <Link to="/">Home </Link></li>
-          <li className="item"><Link to="/about">About </Link></li>
-          <li className="toggle"><a href="#"><i className="fa fa-bars" id="bars"></i></a></li>
-
-        </ul>
-        </nav>
-        <Switch>
-          <Route exact path="/">
-            <Home></Home>
-          </Route>
-          <Route exact path="/about">
-            <About></About>
-          </Route>
-        </Switch>
+        <Navbar></Navbar>
       </header>
     </div>
   );
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,13 +1,33 @@
-import React from "react";
+import {Route, Switch,Link} from "react-router-dom";
+import '../styles/Navbar.css';
+import Home from "../Home";
+import About from "../App";
+import icon from "../assets/Icon-funny.png"
 
-class Navbar extends React.Component{
-    render () {
-        return (
-            <nav>
+function Navbar() {
+    return (
+        <div>
+            <header >
+                <nav>
+                    <ul className="menu">
+                        <li className="logo"><Link to="/"><img className="icon" src={icon} alt="Kitty"/> </Link></li>
+                        <li className="item"> <Link to="/">Home </Link></li>
+                        <li className="item"><Link to="/about">About </Link></li>
+                        <li className="toggle"><a href="#"><i className="fa fa-bars" id="bars"></i></a></li>
 
-            </nav>
-        );
-    }
+                    </ul>
+                </nav>
+                <Switch>
+                    <Route exact path="/">
+                        <Home></Home>
+                    </Route>
+                    <Route exact path="/about">
+                        <About></About>
+                    </Route>
+                </Switch>
+            </header>
+        </div>
+    );
 }
 
 export default Navbar;
